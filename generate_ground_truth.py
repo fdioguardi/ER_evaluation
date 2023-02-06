fb7928759ee888b6c26ef283e56069d8f760af38
Felipe Dioguardi <felipedioguardi@gmail.com>
2023-02-05 22:45:51 -0300

    feat: add flag to allow reproducibility

--- generate_ground_truth.py
+++ generate_ground_truth.py
@@ -1,6 +1,6 @@
 """
-Create a ground truth file of listings and owl:sameAs links between the
-listings.
+Create a ground truth file of listings and `owl:sameAs` links between
+the listings
 """
 import argparse
 import csv
@@ -46,7 +46,7 @@ def main() -> None:
             labels = csv.reader(input_labels)
             labels = [duplicates[0].split(";") for duplicates in labels]
 
-            duplicates, uniques = select_labels(labels)
+            duplicates, uniques = select_labels(labels, args.randomize)
 
             writer = csv.writer(output_labels)
             writer.writerows(format_duplicates(duplicates))
@@ -92,7 +92,9 @@ def format_duplicates(duplicates: list[list[str]]) -> Iterator[list]:
             yield ["+", combination[0], combination[1], 0]
 
 
-def format_non_duplicates(duplicates: list[list[str]], uniques: list[str]) -> Iterator[list]:
+def format_non_duplicates(
+    duplicates: list[list[str]], uniques: list[str]
+) -> Iterator[list]:
     """
     Given a list of duplicates like the following:
         [
@@ -154,7 +156,9 @@ def format_non_duplicates(duplicates: list[list[str]], uniques: list[str]) -> It
         yield _format_non_duplicates(unique[0], unique[1])
 
 
-def select_labels(labels: list[list[str]]) -> tuple[list[list[str]], list[str]]:
+def select_labels(
+    labels: list[list[str]], randomize: bool
+) -> tuple[list[list[str]], list[str]]:
     """
     Given a list of duplicates that contains the following links:
         A <-> B <-> X
@@ -162,43 +166,65 @@ def select_labels(labels: list[list[str]]) -> tuple[list[list[str]], list[str]]:
         E <-> F
         G <-> H
 
-    Remove some of the duplicates (randomly) to ensure the output
+    Remove some of the duplicates to ensure the output
     contains both duplicated and unique listings.
+
+    If `randomize` is True, the label selection will be randomized.
+    Otherwise, the first half of the labels will be selected.
 
     Return a list of links that contains (e.g.) the following links:
         A <-> B <-> X
         E <-> F
     """
-    random.shuffle(labels)
+    if randomize:
+        random.shuffle(labels)
+        uniques: list[str] = [
+            random.choice(dups) for dups in labels[len(labels) // 2 :]
+        ]
+    else:
+        uniques: list[str] = [dups[0] for dups in labels[len(labels) // 2 :]]
 
-    uniques: list[str] = [random.choice(dups) for dups in labels[len(labels) // 2 :]]
     return labels[: len(labels) // 2], uniques
 
 
 def read_args() -> argparse.Namespace:
-    parser: argparse.ArgumentParser = argparse.ArgumentParser(description=__doc__)
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
     parser.add_argument(
-        "--input-labels", default="input/labels.csv", help="Input labels file", type=str
+        "-r",
+        "--randomize",
+        action="store_false",
+        help="randomize the data selection",
+    )
+
+    parser.add_argument(
+        "--input-labels",
+        default="input/labels.csv",
+        help="input labels file",
+        type=str,
     )
     parser.add_argument(
         "--input-data",
         default="input/data.csv",
-        help="Input CSV file containing labeled data",
+        help="input CSV file containing labeled data",
         type=str,
     )
 
     parser.add_argument(
         "--output-labels",
         default="output/labels.csv",
-        help="Output file of owl:sameAs-equivalent links",
+        help="output file of owl:sameAs-equivalent links",
         type=str,
     )
     parser.add_argument(
         "--output-data",
         default="output/data.csv",
-        help="Destination file for the generated dataset.",
+        help="destination file for the generated dataset",
         type=str,
     )
+
     return parser.parse_args()
 
 
